5a6a11e5c9444b91be5db6e3b4fb2aa49565bb8e
Parker Illig <pail4944@colorado.edu>
2016-04-06 23:00:27 -0600

    added better descriptions of tests and compacted asserts

--- tests.js
+++ tests.js
@@ -1,37 +1,18 @@
-QUnit.test("rating of zero gives correct picture", function( assert ) {
-  assert.equal( getstar(0), "<img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>" );
-});
-QUnit.test("rating of 0.5 gives correct picture", function( assert ) {
-  assert.equal( getstar(0.5), "<img src=assets/20x20_1-5.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>" );
-});
-QUnit.test("rating of 1 gives correct picture", function( assert ) {
-  assert.equal( getstar(1), "<img src=assets/20x20_1.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>" );
-});
-QUnit.test("rating of 1.5 gives correct picture", function( assert ) {
-  assert.equal( getstar(1.5), "<img src=assets/20x20_1.png></img><img src=assets/20x20_1-5.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>" );
-});
-QUnit.test("rating of 2 gives correct picture", function( assert ) {
-  assert.equal( getstar(2), "<img src=assets/20x20_2.png></img><img src=assets/20x20_2.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>" );
-});
-QUnit.test("rating of 2.5 gives correct picture", function( assert ) {
-  assert.equal( getstar(2.5), "<img src=assets/20x20_2.png></img><img src=assets/20x20_2.png></img><img src=assets/20x20_2-5.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>" );
-});
-QUnit.test("rating of 3 gives correct picture", function( assert ) {
-  assert.equal( getstar(3), "<img src=assets/20x20_3.png></img><img src=assets/20x20_3.png></img><img src=assets/20x20_3.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>" );
-});
-QUnit.test("rating of 3.5 gives correct picture", function( assert ) {
-  assert.equal( getstar(3.5), "<img src=assets/20x20_3.png></img><img src=assets/20x20_3.png></img><img src=assets/20x20_3.png></img><img src=assets/20x20_3-5.png></img><img src=assets/20x20_0.png></img>" );
-});
-QUnit.test("rating of 4 gives correct picture", function( assert ) {
-  assert.equal( getstar(4), "<img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_0.png></img>" );
-});
-QUnit.test("rating of 4.5 gives correct picture", function( assert ) {
-  assert.equal( getstar(4.5), "<img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4-5.png></img>" );
-});
-QUnit.test("rating of 5 gives correct picture", function( assert ) {
-  assert.equal( getstar(5), "<img src=assets/20x20_5.png></img><img src=assets/20x20_5.png></img><img src=assets/20x20_5.png></img><img src=assets/20x20_5.png></img><img src=assets/20x20_5.png></img>" );
-});
-QUnit.test("Bad ratings give None", function( assert ) {
-  assert.equal( getstar(-1), "None");
-  assert.equal( getstar(6), "None" );
+QUnit.test("getstar should return correct sequence of images", function( assert ) {
+    assert.equal( getstar(0), "<img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>", "gave correct sequence of images for rating of 0");
+    assert.equal( getstar(0.5), "<img src=assets/20x20_1-5.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>", "gave correct sequence of images for rating of 0.5");
+    assert.equal( getstar(1), "<img src=assets/20x20_1.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>", "gave correct sequence of images for rating of 1");
+    assert.equal( getstar(1.5), "<img src=assets/20x20_1.png></img><img src=assets/20x20_1-5.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>", "gave correct sequence of images for rating of 1.5");
+    assert.equal( getstar(2), "<img src=assets/20x20_2.png></img><img src=assets/20x20_2.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>", "gave correct sequence of images for rating of 2");
+    assert.equal( getstar(2.5), "<img src=assets/20x20_2.png></img><img src=assets/20x20_2.png></img><img src=assets/20x20_2-5.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>", "gave correct sequence of images for rating of 2.5");
+    assert.equal( getstar(3), "<img src=assets/20x20_3.png></img><img src=assets/20x20_3.png></img><img src=assets/20x20_3.png></img><img src=assets/20x20_0.png></img><img src=assets/20x20_0.png></img>", "gave correct sequence of images for rating of 3");
+    assert.equal( getstar(3.5), "<img src=assets/20x20_3.png></img><img src=assets/20x20_3.png></img><img src=assets/20x20_3.png></img><img src=assets/20x20_3-5.png></img><img src=assets/20x20_0.png></img>", "gave correct sequence of images for rating of 3.5");
+    assert.equal( getstar(4), "<img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_0.png></img>", "gave correct sequence of images for rating of 4");
+    assert.equal( getstar(4.5), "<img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4.png></img><img src=assets/20x20_4-5.png></img>", "gave correct sequence of images for rating of 4.5");
+    assert.equal( getstar(5), "<img src=assets/20x20_5.png></img><img src=assets/20x20_5.png></img><img src=assets/20x20_5.png></img><img src=assets/20x20_5.png></img><img src=assets/20x20_5.png></img>", "gave correct sequence of images for rating of 5");
+}); 
+QUnit.test("getstar should return None for bad ratings", function( assert ) {
+  	assert.equal( getstar(-1), "None", "Negative rating gave None");
+  	assert.equal( getstar(6), "None" , "Positive out of bounds number gave None");
+    assert.equal( getstar(4.5000000001), "None", "Floating point in range but not exactly one of the 11 possible ratings gave None");
 });
